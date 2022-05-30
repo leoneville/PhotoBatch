f8da6427b28236ba515028fd22685a67f311c518
leoneville <leoneville_@hotmail.com>
2022-05-30 09:52:04 -0300

    initial implementation of the ArgumentParser class

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <iomanip>
 
-int main()
+class ArgumentParser
+{
+public:
+	void RegisterFlag(const std::string& flag)
+	{
+
+	}
+
+	bool GetFlag(const std::string& flag)
+	{
+		return false;
+	}
+
+	void Parse(int argc, char* argv[])
+	{
+
+	}
+
+private:
+
+};
+
+int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "portuguese");
 
-	std::cout << "Olá, Mundo!\n";
+	auto argParser = ArgumentParser{};
+	argParser.RegisterFlag("rename");
+	argParser.RegisterFlag("convert");
+	argParser.RegisterFlag("resize");
+	argParser.RegisterFlag("scale");
+
+	argParser.Parse(argc, argv);
+
+	std::cout << "Rename : " << std::boolalpha << argParser.GetFlag("rename") << std::endl;
+	std::cout << "Convert: " << std::boolalpha << argParser.GetFlag("convert") << std::endl;
+	std::cout << "Resize : " << std::boolalpha << argParser.GetFlag("resize") << std::endl;
+	std::cout << "Scale  : " << std::boolalpha << argParser.GetFlag("scale") << std::endl;
+
 
 	return 0;
 }
